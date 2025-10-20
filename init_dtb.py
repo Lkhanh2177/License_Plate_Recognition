f1a607db9bf12970b938d5597fb9b37892b72c39
Lkhanh2177 <lkhanh2177@gmail.com>
2025-10-20 22:10:55 +0700

    update: add columns in database

--- init_dtb.py
+++ init_dtb.py
@@ -2,20 +2,20 @@ import sqlite3
 
 DB_PATH = "license_plates.db"
 
-conn = sqlite3.connect(DB_PATH)
-
-cursor = conn.cursor()
-
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS detections (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        start_time TEXT,
-        end_time TEXT,
-        plate_origin TEXT,
-        plate_text TEXT,
-        image_crop BLOB
-    )
-""")
-
-conn.commit()
-conn.close()
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS detections (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                plate_origin TEXT,
+                plate_text TEXT,
+                start_time TEXT,
+                end_time TEXT,
+                image_crop BLOB,
+                direction TEXT,
+                camera_location TEXT
+            )
+        ''')
+    conn.commit()
+    conn.close()
